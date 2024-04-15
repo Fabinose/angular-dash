be5488f8abb92322e93fef4abb095335cc8fa45f
fabinose <sarhankfabi@gmail.com>
2024-04-15 16:42:39 +0530

    first

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { DailyTaskComponent } from './daily-task/daily-task.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'home',
+        component: HomeComponent,
+    },
+    {
+        path: 'daily-task',
+        loadComponent: () => import('./daily-task/daily-task.component').then((c) => c.DailyTaskComponent),
+    }
+];
---
+++ src/app/daily-task/daily-task.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-daily-task',
+  standalone: true,
+  imports: [],
+  templateUrl: './daily-task.component.html',
+  styleUrl: './daily-task.component.css'
+})
+export class DailyTaskComponent {
+
+}
